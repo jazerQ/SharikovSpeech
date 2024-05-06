1f6246409fb2d7566a8c787c8d73559da0645348
Your Name <you@example.com>
2024-05-06 20:28:01 +0300

    fc

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace MyFirstApp {
+    class Program {
+        static void Main(string[] args)
+        {
+
+            //Thread.Sleep(1000);
+            Console.WriteLine("Введи то, чему хочешь научить Шарикова");
+            string name = Console.ReadLine();
+            string revName = new string(name.Reverse().ToArray());
+            //Thread.Sleep(1000);
+            //Console.WriteLine(revName);
+            string face1 = " ----------\n|         |\n|o       o|\n|         |\n|   ----  |\n ----------";
+            string face2 = " ----------\n|         |\n|o       o|\n|         |\n|    |O|  |\n ----------";
+            int s = 0;
+            string ss = "";
+            while(s != revName.Length)
+            {
+                ss += revName[s];
+                Console.Clear();
+                if (s % 2 == 0)
+                {
+                    Console.WriteLine(face1);
+                    Console.WriteLine(ss);
+                    s++;
+                    Thread.Sleep(400);
+
+                }
+                else
+                {
+                    Console.WriteLine(face2);
+                    Console.WriteLine(ss);
+                    s++;
+                    Thread.Sleep(400);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
